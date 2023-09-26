0c3b381baf461e000fdefa96162991c1cde37396
Barry <S1186391@student.windesheim.nl>
2023-09-26 23:53:42 +0200

    kaas

--- index.php
+++ index.php
@@ -1,36 +1,69 @@
 <?php
 date_default_timezone_set("Europe/Amsterdam");
 
-$teammembers = ["1" => "arie",
-    "2" => "polina",
-    "3" => "stephan"
+
+$team = [
+    //firstname
+    'Rowin' => [
+        'Surname'   => 'van Gils',
+        'DoB'       => '20/05/2004',
+        'Age'       => 19,
+        'Height'    => 1.73,
+        'IsStudent' => true,
+        'Grades'    => [
+            'Frontend'      => 7.8,
+            'Backend'       => 8.0,
+            'Vaardigheden'  => 8
+        ],
+        'FavColors' => [
+            'Blue',
+            'Purple'
+        ],
+        'Hobbies'   => [
+            'Skating',
+            'Gaming',
+            'Guitar'
+        ],
+        'Contact'   => 'rowinvgils@gmail.com',
+        'address'   => 'nergens',
+    ],
+
+    'Barry' => [
+        'Surname'   => 'van den Berg',
+        'Dob'       => '18/03/2004',
+        'Age'       => 19,
+        'Height'    => 1.83,
+        'IsStudent' => true,
+        'Grades'    => [
+            'Frontend'      => 6.0,
+            'Backend'       => 8.0,
+            'Vaardigheden'  => 1.0
+        ],
+        'FavColors' => [
+            'Aquamarine',
+            'Beaver' //de kleur niet het dier
+        ],
+        'Hobbies'   => [
+            'Gamen',
+            'Muziek'
+        ],
+        'Contact'   => 'barryvdnberg@gmail.com',
+        'address'   => 'alkmaar'
+    ]
 ];
 
-$details = [
-    1 => [1 => "pv"],
-    2 => [1 => "sl01"],
-    3 => [2 => "sl01"],
-    4 => [3 => "fd"],
-    5 => [3 => "sl01"]
-];
-
-//foreach($teammembers as $member => $modules) {
-//    echo "<div>
-//            <h3>
-//                <a href='views/details.view.php'>$member</a>
-//            </h3>
-//            <p>";
+foreach($team as $member => $modules) {
+    $v ='/views/details.view.php?var='.$member;
+    echo "<div>
+            <h3>
+                
+                <a href='$v'>$member</a>
+            </h3>
+            <p>";
 //    foreach($modules as $module) {
 //        echo $module . "<br>";
 //    }
 //    echo "</p></div>";
-//
-//}
 
-foreach ($details as $detail => $modules) {
-    if($detail == 1) {
-        foreach ($modules as $module) {
-            echo $module . "<br>";
-        }
-    }
 }
+
--- views/details.view.php
+++ views/details.view.php
@@ -3,5 +3,27 @@
  * Created by: Stephan Hoeksema 2023
  * SL01COL03
  */
+include($_SERVER['DOCUMENT_ROOT'].'\\index.php');
+$t=$_GET['var'];
+displayComplexArray($team[$t]);
+
+function displayComplexArray($array, $indent = 0)
+{
+    foreach ($array as $key => $value) {
+        //extra break, because why not
+        if($indent == 0) {echo"<br>";}
+        //indentation for display
+        echo str_repeat("&nbsp; &nbsp;", $indent);
+
+        if (is_array($value)) {
+            // if the value is an array, recursion :P
+            echo "$key:<br>";
+            displayComplexArray($value, $indent + 1);
+        } else {
+            // if it's not an array, display as normal
+            echo "$key: $value<br>";
+        }
+    }
+}/**/
 
 echo "het werkt!";
